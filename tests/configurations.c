3bd9df272ffcfa03ce8cd495a3225f3f9dc9bb3f
Alexander Brandt <brandt.alxndr@outlook.com>
2020-04-11 21:29:39 -0300

    [Tests] MSVC doesn't like generics - Episode Two -

--- tests/configurations.c
+++ tests/configurations.c
@@ -54,19 +54,19 @@ void ConfigTest1(void** cmocka_state)
 		const char* s;
 	} value;
 
-	jaCvarValue(jaCvarGet(cfg, "sound.volume"), &value.f, NULL);
+	jaCvarValueFloat(jaCvarGet(cfg, "sound.volume"), &value.f, NULL);
 	assert_true((value.f == 0.8f)); // Value of "  +0.4 6" can't be cast into a float
 
-	jaCvarValue(jaCvarGet(cfg, "name"), &value.s, NULL);
+	jaCvarValueString(jaCvarGet(cfg, "name"), &value.s, NULL);
 	assert_string_equal(value.s, "OwO");
 
-	jaCvarValue(jaCvarGet(cfg, "render.width"), &value.i, NULL);
+	jaCvarValueInt(jaCvarGet(cfg, "render.width"), &value.i, NULL);
 	assert_int_equal(value.i, 200); // Round of value "   200.2  "
 
-	jaCvarValue(jaCvarGet(cfg, "render.height"), &value.i, NULL);
+	jaCvarValueInt(jaCvarGet(cfg, "render.height"), &value.i, NULL);
 	assert_int_equal(value.i, 320); // Value of " UwU  " can't be cast into a integer
 
-	jaCvarValue(jaCvarGet(cfg, "render.fullscreen"), &value.i, NULL);
+	jaCvarValueInt(jaCvarGet(cfg, "render.fullscreen"), &value.i, NULL);
 	assert_int_equal(value.i, 1); // Value of "2" gets clamp
 
 	// Bye!
@@ -139,15 +139,15 @@ void ConfigTest3_ParseFile(void** cmocka_state)
 	struct jaConfiguration* cfg = jaConfigurationCreate();
 	FILE* fp = fopen("./tests/parse-test.jcfg", "rb");
 
-	jaCvarCreate(cfg, "osc.shape", "square", NULL, NULL, NULL);
-	jaCvarCreate(cfg, "osc.frequency", 220, INT_MIN, INT_MAX, NULL);
-	jaCvarCreate(cfg, "osc.sub_volume", 0.5f, 0.0f, 1.0f, NULL);
+	jaCvarCreateString(cfg, "osc.shape", "square", NULL, NULL, NULL);
+	jaCvarCreateInt(cfg, "osc.frequency", 220, INT_MIN, INT_MAX, NULL);
+	jaCvarCreateFloat(cfg, "osc.sub_volume", 0.5f, 0.0f, 1.0f, NULL);
 
-	jaCvarCreate(cfg, "env.attack", 50.0f, 0.0f, 1000.0f, NULL);
-	jaCvarCreate(cfg, "env.decay", 0.0f, 0.0f, 1000.0f, NULL);
-	jaCvarCreate(cfg, "env.sustain", 1.0f, 0.0f, 1.0f, NULL);
-	jaCvarCreate(cfg, "env.hold", 0.0f, 0.0f, 1000.0f, NULL);
-	jaCvarCreate(cfg, "env.release", 200.0f, 0.0f, 1000.0f, NULL);
+	jaCvarCreateFloat(cfg, "env.attack", 50.0f, 0.0f, 1000.0f, NULL);
+	jaCvarCreateFloat(cfg, "env.decay", 0.0f, 0.0f, 1000.0f, NULL);
+	jaCvarCreateFloat(cfg, "env.sustain", 1.0f, 0.0f, 1.0f, NULL);
+	jaCvarCreateFloat(cfg, "env.hold", 0.0f, 0.0f, 1000.0f, NULL);
+	jaCvarCreateFloat(cfg, "env.release", 200.0f, 0.0f, 1000.0f, NULL);
 
 	if (jaConfigurationFileEx(cfg, fp, FILE_DEFAULT, sTokenizerCallback, sWarningsCallback, &st) != 0)
 		jaStatusPrint("", st);
